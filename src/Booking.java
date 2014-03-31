2163bca540c2912e3e0adf862a3946852aa7ec4d
sohaibmushtaq <sohaibmushtaq@sohaibmushtaqs-MacBook-Pro.local>
2014-03-31 21:52:05 +1100

    further changes

--- src/Booking.java
+++ src/Booking.java
@@ -1,4 +1,41 @@
+import java.util.Date;
+
 
 public class Booking {
 
+	public String aName;
+	
+	public int aCapacity;
+	
+	public Date StartDate;
+	
+	public Date EndDate;
+	
+	//book hotel
+	public Hotel newBook(String Name, Date SDate, Date EDate){
+		
+		Hotel BookedRoom = null;
+		
+		
+		
+		return BookedRoom;
+	}
+	
+	//function to check of room avaliable
+	public boolean HotelisAvaliable(Hotel aHotel, Date SDate, Date EDate){
+		
+		return true;
+	}
+	
+	//check if date is valid
+	public boolean DateisValid(Date SDate, Date EDate) {
+		
+		return true;
+	}
+	
+	
 }
+
+
+//function to check of room avaliable
+	
--- src/HotelBookingSystem.java
+++ src/HotelBookingSystem.java
@@ -57,4 +57,5 @@ public class HotelBookingSystem {
 			aHotel.add(r);
 		}
 	}
+
 }
